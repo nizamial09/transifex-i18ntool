54359d48a66a8e742f3873595d9a5f80b374d196
Sebastian Krah <exotelis@mailbox.org>
2019-03-13 10:20:59 +0100

    Initial commit
    Add binary
    
    Signed-off-by: Sebastian Krah <exotelis@mailbox.org>

---
+++ bin/i18ntool.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const i18ntool = require('../lib');
+const util = require('../lib/util');
+const minimist = require('minimist');
+
+let argv = minimist(process.argv.slice(2), {
+  string: ['c', 'srcDir', 'tarDir', 'srcFile'],
+  boolean: ['v', 'removeUnusedIds', 'q'],
+  alias: util.aliases(),
+  default:  {
+    srcDir: '.',
+    tarDir: '.',
+    srcFile: 'messages.xlf',
+    removeUnusedIds: true,
+    q: false
+  }
+});
+
+// Parse arguments
+if (typeof argv['_'][0] === 'undefined' || argv['_'].length === 0) {
+  // No arguments given
+
+  if (argv["version"]) {
+    // If -v or --version was passed, show the version of the CLI
+    i18ntool.version();
+  } else {
+    // Otherwise, just show the help screen
+    i18ntool.help();
+  }
+} else {
+  // Arguments given
+
+  // Apply config file
+  try {
+    util.applyConfig(argv);
+  } catch (e) {
+    process.stdout.write(e.message + '\n');
+    process.exit();
+  }
+
+  if (typeof i18ntool[argv['_'][0]] === 'undefined') {
+    // If the command doesn't exist, show the help screen
+    i18ntool.help();
+  } else {
+    // Execute command
+    i18ntool[argv['_'][0]](argv);
+  }
+}
